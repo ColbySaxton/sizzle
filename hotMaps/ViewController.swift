64f2cbe5aca19bdb85e9d9c5c6164275e7c26632
ColbySaxton <cas264@case.edu>
2019-02-16 12:27:11 -0500

    Initial Commit

---
+++ hotMaps/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hotMaps
+//
+//  Created by Colby Saxton on 2/16/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

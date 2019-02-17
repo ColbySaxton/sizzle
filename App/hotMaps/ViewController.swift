94b6ae26704df4a7bee3f557239f408c91770c00
ColbySaxton <cas264@case.edu>
2019-02-16 23:43:09 -0500

    changed map icons and full but static menu

--- App/hotMaps/ViewController.swift
+++ App/hotMaps/ViewController.swift
@@ -40,12 +40,12 @@ class ViewController: UIViewController {
         marker.map = mapView
         
         
-        addMarker(map: mapView, position: CLLocationCoordinate2D(latitude: 41.511130, longitude: -81.602635), title: "happyDog", desc: "fools gold", venueType: 1)
+        addMarker(map: mapView, position: CLLocationCoordinate2D(latitude: 41.511130, longitude: -81.602635), title: "happyDog", desc: "fools gold", liveNum: 1)
         
-         addMarker(map: mapView, position: CLLocationCoordinate2D(latitude: 41.509197, longitude: -81.605424), title: "Sports Bar: Corner Alley" , desc: "Location: 402 Euclid Ave, Cleveland, OH 44114 \nTime: 9:00pm - 2:00am \nDescription: Come join us in the heart of downtown as we watch the final regular season game for the Cleveland Cavaliers! Defend the LAND!", venueType: 1)
-        addMarker(map: mapView, position: CLLocationCoordinate2D(latitude: 41.508736, longitude: -81.604605), title: "Hotel Bar: The Vault", desc: "Location: 2017 E 9th St, Cleveland, OH 44115\nTime: 8:00pm - 2:30am \nDescription: Travel back in time and experience the glamor and luxury of a classic 1800s Cleveland bank. Surround yourself in elegance, history, and incredible hand-crafted cocktails.", venueType: 2)
-        addMarker(map: mapView, position: CLLocationCoordinate2D(latitude: 41.507441, longitude: -81.608250), title: "Music: Drag Queen Show @ The Jolly Scholar", desc: "Location: Thwing Center, 11111 Euclid Ave, Cleveland, OH 44106\nTime: 8:00pm - 10:00pm\nDescription: Catch Rupaul’s, “A Drag Queen’s Christmas” with discounted prices exclusively on Habanero! ", venueType: 5)
-        addMarker(map: mapView, position: CLLocationCoordinate2D(latitude: 41.500801    , longitude: -81.592810), title: "Specialty: Fairmount Bar", desc: "Location: 2448 Fairmount Blvd, Cleveland, OH 44106\nTime: 8:00pm - 1:30am\nDescription: Enjoy Cleveland’s best made to order drinks, with over fifty beers on tap. Immerse yourself in this young and upcoming area in Cleveland Heights!", venueType: 8)
+         addMarker(map: mapView, position: CLLocationCoordinate2D(latitude: 41.509197, longitude: -81.605424), title: "Sports Bar: Corner Alley" , desc: "Location: 402 Euclid Ave, Cleveland, OH 44114 \nTime: 9:00pm - 2:00am \nDescription: Come join us in the heart of downtown as we watch the final regular season game for the Cleveland Cavaliers! Defend the LAND!", liveNum: 10)
+        addMarker(map: mapView, position: CLLocationCoordinate2D(latitude: 41.508736, longitude: -81.604605), title: "Hotel Bar: The Vault", desc: "Location: 2017 E 9th St, Cleveland, OH 44115\nTime: 8:00pm - 2:30am \nDescription: Travel back in time and experience the glamor and luxury of a classic 1800s Cleveland bank. Surround yourself in elegance, history, and incredible hand-crafted cocktails.", liveNum: 60)
+        addMarker(map: mapView, position: CLLocationCoordinate2D(latitude: 41.507441, longitude: -81.608250), title: "Music: Drag Queen Show @ The Jolly Scholar", desc: "Location: Thwing Center, 11111 Euclid Ave, Cleveland, OH 44106\nTime: 8:00pm - 10:00pm\nDescription: Catch Rupaul’s, “A Drag Queen’s Christmas” with discounted prices exclusively on Habanero! ", liveNum: 60)
+        addMarker(map: mapView, position: CLLocationCoordinate2D(latitude: 41.500801    , longitude: -81.592810), title: "Specialty: Fairmount Bar", desc: "Location: 2448 Fairmount Blvd, Cleveland, OH 44106\nTime: 8:00pm - 1:30am\nDescription: Enjoy Cleveland’s best made to order drinks, with over fifty beers on tap. Immerse yourself in this young and upcoming area in Cleveland Heights!", liveNum: 20)
         
     }
 
@@ -66,31 +66,22 @@ class ViewController: UIViewController {
         menuShow = !menuShow
     }
     
-    private func addMarker(map: GMSMapView, position: CLLocationCoordinate2D, title: String, desc: String, venueType: Int){
+    private func addMarker(map: GMSMapView, position: CLLocationCoordinate2D, title: String, desc: String, liveNum: Int){
 
         
         
         var ic : UIImage
         let marker = GMSMarker(position: position)
+        
 
-        switch(venueType){
+        switch(liveNum){
             //load different icon for  each venue (custom markers)
-        case 1:
-            ic = GMSMarker.markerImage(with: UIColor.blue)
-        case 2:
-            ic = GMSMarker.markerImage(with: UIColor.init(red: 0.0, green: 0.0, blue: 0.75, alpha: 1.0))
-        case 3:
-            ic = GMSMarker.markerImage(with: UIColor.init(red: 0.0, green: 0.0, blue: 0.5, alpha: 1.0))
-        case 4:
-            ic = GMSMarker.markerImage(with: UIColor.init(red: 0.0, green: 0.0, blue: 0.25, alpha: 1.0))
-        case 5:
-            ic = GMSMarker.markerImage(with: UIColor.init(red: 0.25, green: 0.0, blue: 0.0, alpha: 1.0))
-        case 6:
-            ic = GMSMarker.markerImage(with: UIColor.init(red: 0.5, green: 0.0, blue: 0.0, alpha: 1.0))
-        case 7:
-            ic = GMSMarker.markerImage(with: UIColor.init(red: 0.75, green: 0.0, blue: 0.0, alpha: 1.0))
-        case 8:
-            ic = GMSMarker.markerImage(with: UIColor.init(red: 1.0, green: 0.0, blue: 0.0, alpha: 1.0))
+        case 0..<15:
+            ic = UIImage(named: "Blue LOW Circle")!
+        case 15..<40:
+            ic = UIImage(named: "Orange MEDIUM circle")!
+        case 40..<100:
+            ic = UIImage(named: "Red HOT circle")!
         default:
             //todo error log here for a bad venue type
             ic = GMSMarker.markerImage(with: UIColor.red)

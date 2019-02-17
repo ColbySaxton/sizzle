4b82d499e0d9f9c6dbfc9d4645de6648b9dcf1a3
root <root@DESKTOP-TSHPIQ4.localdomain>
2019-02-16 23:31:55 -0500

    socket comms initial setup

--- hsserver/client.py
+++ hsserver/client.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3.6
 
 import socket
-
-HOST = "172.19.37.18"
+import json
+HOST = "127.0.0.1"
 PORT = 8000
-HOTSPOT = 'N 75 W 876'
+HOTSPOT = json.dumps({'getevents':0})
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
 	s.connect((HOST, PORT))
 	s.sendall(HOTSPOT.encode())
-	data = s.recv(1024).decode()
+	data = s.recv(1024*8)
+	print(data.decode())
 
-print('Received: {}'.format(data))
